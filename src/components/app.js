23eb7c80f1f85bd965c6842c2839deffed6d0e80
SuperSaiyanTizz <supersaiyantizz4@gmail.com>
2022-04-24 07:30:22 -0700

    generated project

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <div>
+          logo
+        </div>
+        <h2>React Redux Router</h2>
+      </div>
+    );
+  }
+}
