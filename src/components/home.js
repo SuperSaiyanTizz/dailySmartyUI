16fc61fb1b3696dcd14e3569beadf5f968622abb
SuperSaiyanTizz <supersaiyantizz4@gmail.com>
2022-05-26 19:31:59 -0700

    fixed some grid styles

--- src/components/home.js
+++ src/components/home.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import Logo from './logo';
 import SearchBar from './searchBar';
-import RecentPosts from './recentPost';
+import RecentPosts from './recentPosts';
 
-export default class Home extends Component {
-  
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class Home extends Component {
+
   handleSearchBarchSubmit(query) {
-    console.log('trying to handle submit query', query);
+    this.props.fetchPostsWithQuery(query, () => {
     this.props.history.push('/results');
+    });
+    
 
   }
-  
+
   render() {
     return (
-      <div className='home'>
-        <div>
-          <Logo/>
-          <SearchBar onSubmit={(query)=> this. handleSearchBarchSubmit(query)}/>
-          <RecentPosts/>
-        </div>
-         </div>
+      <div className="home">
+       <Logo />
+       <SearchBar onSubmit={(query) => this.handleSearchBarchSubmit(query)} />
+       <RecentPosts />
+      </div>
     );
   }
 }
+
+export default connect(null, actions)(Home);
--- src/components/post.js
+++ src/components/post.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import AnimateHeight from 'react-animate-height';
 
 class Post extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            height: 0
+        }
+
+    }
 
     renderTopics() {
         let topics = this.props.associated_topics.map((topic, index) => {
@@ -9,9 +19,24 @@ class Post extends Component {
         return topics;
     }
 
+    renderLinks() {
+        let links = this.props.post_links.map((post_link, index)=> {
+          return (
+            <div className="post-link" key={index}>
+            <div className="post-link__box"></div>
+             <div className="post-link__link">
+          <a href={post_link.link_url}>Useful Link #{index + 1}</a>
+             </div>
+            </div>
+          )
+        })
+        return links;
+    }
+
     render() {
-        return (
-            <li className="recent-post">
+        if (this.props.type== 'recent') {
+            return (
+                <li className="recent-post">
                 <div className="recent-post__title">
                     {this.props.title}
                 </div>
@@ -19,5 +44,33 @@ class Post extends Component {
                     {this.renderTopics()}
                 </div>
             </li>
-      )}
+
+            )
+        }else if (this.props.type == 'result') {
+            return (
+                <li className="result-post">
+                    <div className="recent-post_topics">
+                    {this.renderTopics()} 
+                    </div>
+                    <div className="recent-post_title">
+            <a href={this.props.url_for_post}
+                onMouseEnter={()=> this.setState({height: 70})}
+                onMouseLeave={() => this.setState({height: 0})}
+            >
+            {this.props.title}</a>
+                    </div>
+                    <AnimateHeight
+                        duration={500}
+                        height={this.state.height}
+                    >
+                    <div className="result-post_links">
+                    {this.renderLinks()}
+                    </div>
+                    </AnimateHeight>
+                   </li>
+            )
+        }
     }
+}
+    
+export default Post;
